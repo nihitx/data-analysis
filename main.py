b6d1e300c27a0c266a335bf6822087c0122ff574
nihitx <masnadhossain@live.com>
2017-03-18 18:10:26 +0200

    enrollment cleaned up

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import unicodecsv
+from datetime import datetime as dt
 
 enrollments_filename = 'data/enrollments.csv'
 engagement_filename = 'data/daily_engagement.csv'
@@ -22,3 +23,28 @@ with open(submissions_filename, 'rb') as f:
     submission = list(reader)
 print(submission[0])
 print('---------------')
+
+# now lets update the values of enrolment for all strings, eg is_canceled should be boolean and cancel_date should be date
+
+def parse_date(date):
+    if date == '':
+        return None
+    else:
+        return dt.strptime(date, '%Y-%m-%d')
+
+def if_int(i):
+    if i == '':
+        return None
+    else:
+        return int(i)
+
+
+for enrollment in enrollments:
+    enrollment['cancel_date'] = parse_date(enrollment['cancel_date'])
+    enrollment['days_to_cancel'] = if_int(enrollment['days_to_cancel'])
+    enrollment['is_canceled'] = enrollment['is_canceled'] == 'True'
+    enrollment['is_udacity'] = enrollment['is_udacity'] == 'True'
+    enrollment['join_date'] = parse_date(enrollment['join_date'])
+
+print(enrollments[0])
+print('------enrollment cleaned up -----------')
